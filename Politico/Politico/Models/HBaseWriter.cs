32b48a65d68e39ce041cddf9a7e1289574f575f4
alleeclark <allee.clark@bison.howard.edu>
2016-03-26 01:58:30 -0400

    Create Twitter streaming service

---
+++ Politico/Politico/Models/HBaseWriter.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.HBase.Client;
+using org.apache.hadoop.hbase.rest.protobuf.generated;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tweetinvi.Core.Interfaces;
+
+namespace AngularJSWebApiEmpty.Models
+{
+    class HBaseWriter
+    {
+        const string CLUSTERNAME = "";
+        const string HADOOPUSERNAME = "admin";
+        const string HADOOPPASSWORD = "Nopassword";
+        const string HBASETABLENAME = "blacktweets";
+
+        const string DICTIONARYFILENAME = @"..\..\dictionary.tsv";
+        private static char[] _punctuationChars = new[] {
+    ' ', '!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',   //ascii 23--47
+    ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~' };
+
+        HBaseClient client;
+        Dictionary<string, DictionaryItem> dictionary;
+
+        Thread writerThread;
+        Queue<ITweet> queue = new Queue<ITweet>();
+        bool threadRunning = true;
+
+        public HBaseWriter()
+        {
+            ClusterCredentials credentials = new ClusterCredentials(new Uri(CLUSTERNAME), HADOOPUSERNAME, HADOOPPASSWORD);
+            client = new HBaseClient(credentials);
+
+            if (!client.ListTables().name.Contains(HBASETABLENAME))
+            {
+                TableSchema tableSchema = new TableSchema();
+                tableSchema.name = HBASETABLENAME;
+                tableSchema.columns.Add(new ColumnSchema { name = "d" });
+                client.CreateTable(tableSchema);
+                Console.WriteLine("Table \"{0}\" is created", HBASETABLENAME);
+            }
+
+            LoadDictionary();
+
+            writerThread = new Thread(new ThreadStart(WriterThreadFunction));
+            writerThread.Start();
+        }
+
+        ~HBaseWriter()
+        {
+            threadRunning = false;
+        }
+
+        public void WriteTweet(ITweet tweet)
+        {
+            lock (queue)
+            {
+                queue.Enqueue(tweet);
+            }
+        }
+
+        private void LoadDictionary()
+        {
+            List<string> lines = File.ReadAllLines(DICTIONARYFILENAME).ToList();
+            var items = lines.Select(line =>
+            {
+                var fields = line.Split('\t');
+                var pos = 0;
+                return new DictionaryItem
+                {
+                    Type = fields[pos++],
+                    Length = Convert.ToInt32(fields[pos++]),
+                    Word = fields[pos++],
+                    Pos = fields[pos++],
+                    Stemmed = fields[pos++],
+                    Polarity = fields[pos++]
+                };
+            });
+
+            dictionary = new Dictionary<string, DictionaryItem>();
+            foreach (var item in items)
+            {
+                if (!dictionary.Keys.Contains(item.Word))
+                {
+                    dictionary.Add(item.Word, item);
+                }
+            }
+        }
+
+        private int CalcSentimentScore(string[] words)
+        {
+            Int32 total = 0;
+            foreach (string word in words)
+            {
+                if (dictionary.Keys.Contains(word))
+                {
+                    switch (dictionary[word].Polarity)
+                    {
+                        case "negative": total -= 1; break;
+                        case "positive": total += 1; break;
+
+                    }
+                }
+            }
+            if (total > 0)
+            {
+                return 1;
+            }
+            else if (total < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void CreateTweetByWordsCell(CellSet set, ITweet tweet)
+        {
+            string[] words = tweet.Text.ToLower().Split(_punctuationChars);
+            int sentimentScore = CalcSentimentScore(words);
+            var words_pairs = words.Take(words.Length - 1)
+                                .Select((word, idx) => string.Format("{0} {1}", words, words[idx + 1]));
+            var all_words = words.Concat(words_pairs).ToList();
+
+            foreach (string word in all_words)
+            {
+                string time_index = (ulong.MaxValue - (ulong)tweet.CreatedAt.ToBinary()).ToString().PadLeft(20) + tweet.IdStr;
+                string key = word + "_" + time_index;
+
+                var row = new CellSet.Row { key = Encoding.UTF8.GetBytes(key) };
+
+                var value = new Cell { column = Encoding.UTF8.GetBytes("d:id_str"), data = Encoding.UTF8.GetBytes(tweet.IdStr) };
+                row.values.Add(value);
+
+                value = new Cell { column = Encoding.UTF8.GetBytes("d:lang"), data = Encoding.UTF8.GetBytes(tweet.Language.ToString()) };
+                row.values.Add(value);
+
+                if (tweet.Hashtags != null)
+                {
+                    var str = tweet.Hashtags.ToString();
+                    value = new Cell { column = Encoding.UTF8.GetBytes("d:hash"), data = Encoding.UTF8.GetBytes(str) };
+                    row.values.Add(value);
+                }
+
+                value = new Cell { column = Encoding.UTF8.GetBytes("d:sentiment"), data = Encoding.UTF8.GetBytes(sentimentScore.ToString()) };
+                row.values.Add(value);
+
+                set.rows.Add(row);
+            }
+        }
+
+        public void WriterThreadFunction()
+        {
+            try
+            {
+                while (threadRunning)
+                {
+                    if (queue.Count > 0)
+                    {
+                        CellSet set = new CellSet();
+                        lock (queue)
+                        {
+                            do
+                            {
+                                ITweet tweet = queue.Dequeue();
+                                CreateTweetByWordsCell(set, tweet);
+                            } while (queue.Count > 0);
+                        }
+                        client.StoreCells(HBASETABLENAME, set);
+                        Console.WriteLine("\tRows written: {0}", set.rows.Count);
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+        }
+        public class DictionaryItem
+        {
+            public string Type { get; set; }
+            public int Length { get; set; }
+            public string Word { get; set; }
+            public string Pos { get; set; }
+            public string Stemmed { get; set; }
+            public string Polarity { get; set; }
+
+        }
+    }
+}
