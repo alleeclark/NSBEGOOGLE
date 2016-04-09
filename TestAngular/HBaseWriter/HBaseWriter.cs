e412067b2c983590d94c7a548db2aec840dc73ce
alleeclark <allee.clark@bison.howard.edu>
2016-04-09 13:00:36 -0400

    Finished HBaseWriter & Updated Methods from package

--- TestAngular/HBaseWriter/HBaseWriter.cs
+++ TestAngular/HBaseWriter/HBaseWriter.cs
@@ -2,6 +2,8 @@
 using org.apache.hadoop.hbase.rest.protobuf.generated;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -29,23 +31,25 @@ namespace HBaseWriter
         public HBaseWriter()
         {
             var credenitals = CreateFromFile(@"..\..\credentials.txt");
-                client = new HBaseClient(credenitals);
+            client = new HBaseClient(credenitals);
+            TableList tables = client.ListTablesAsync().Result;
 
-            if(!client.ListTables().name.Contains(TABLE_BY_WORDS_NAME))
-            {
+            //if (!client.ListTablesAsync().name.Contains(TABLE_BY_WORDS_NAME))
+            if(!tables.name.Contains(TABLE_BY_WORDS_NAME))    
+                {
                 var tableSchema = new TableSchema();
                 tableSchema.name = TABLE_BY_WORDS_NAME;
                 tableSchema.columns.Add(new ColumnSchema { name = "d" });
-                client.CreateTable(tableSchema);
+                client.CreateTableAsync(tableSchema);// CreateTable(tableSchema);
                 Console.WriteLine("Table \"{0}\" created.", TABLE_BY_WORDS_NAME);
- 
+
             }
 
             rowCount = GetRowCount();
 
             LoadDictionary();
 
-            writerThread = new Thread(new ThreadStart(writerThreadFunction));
+            writerThread = new Thread(new ThreadStart(WriteThreadFunction));
             writerThread.Start();
         }
 
@@ -56,18 +60,20 @@ namespace HBaseWriter
 
         private long GetRowCount()
         {
-            try {
-                var cellSet = client.GetCells(TABLE_BY_WORDS_NAME, COUNT_ROW_KEY);
+            try
+            {
+                var cellSet = client.GetCellsAsync(TABLE_BY_WORDS_NAME, COUNT_ROW_KEY).Result;
+
                 if (cellSet.rows.Count != 0)
                 {
-                    var count = cellSet.rows[0].values.Find(cell => Encoding.UTF8.GetString(cell.colum) == COUNT_COLUMN_NAME);
+                    var countCol = cellSet.rows[0].values.Find(cell => Encoding.UTF8.GetString(cell.column) == COUNT_COLUMN_NAME);
                     if (countCol != null)
                     {
                         return Convert.ToInt64(Encoding.UTF8.GetString(countCol.data));
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return 0;
             }
@@ -90,33 +96,33 @@ namespace HBaseWriter
 
         public void WriteTweet(Tweetinvi.Core.Interfaces.ITweet tweet)
         {
-            lock(queue)
+            lock (queue)
             {
                 queue.Enqueue(tweet);
             }
         }
 
-        public WriteThreadFunction()
+        public void WriteThreadFunction()
         {
-            while(threadRunning)
+            while (threadRunning)
             {
                 try
                 {
-                    if(queue.Count > 0)
+                    if (queue.Count > 0)
                     {
                         var set = new CellSet();
-                        lock(queue)
+                        lock (queue)
                         {
                             do
                             {
                                 var tweet = queue.Dequeue();
 
-                                CreateTweetByWordsCells(set, tweet);
+                                CreateTweetByWordCells(set, tweet);
                             } while (queue.Count > 0);
                         }
                         CreateRowCountCell(set, rowCount + set.rows.Count);
-
-                        client.StoreCells(TABLE_BY_WORDS_NAME, set);
+                        client.StoreCellsAsync(TABLE_BY_WORDS_NAME, set);
+                        //client.StoreCells(TABLE_BY_WORDS_NAME, set);
 
                         rowCount += set.rows.Count;
 
@@ -124,7 +130,7 @@ namespace HBaseWriter
                     }
                     Thread.Sleep(100);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     Console.WriteLine("Exception: " + ex.Message + "\nStackTrace \n" + ex.StackTrace);
                 }
@@ -139,30 +145,127 @@ namespace HBaseWriter
         {
             var words = tweet.Text.ToLower().Split(_punctuationChars);
             int sentimentScorer = CalcSentimentScore(words);
-            var word_pairs = words.Take(words.Length - 1) Select((words, idx) => string.Format("{0} {1", words, words[idx + 1]));
+            var word_pairs = words.Take(words.Length - 1) .Select((word, idx) => string.Format("{0} {1", words, words[idx + 1]));
             var all_words = words.Concat(word_pairs).ToList();
 
-            foreach(var word in all_words)
+            foreach (var word in all_words)
             {
-                var time_index =(ulong.MaxValue - (ulong)tweet.CreatedAt.ToBinary()).ToString().PadLeft(20) + tweet.IdStr;
+                var time_index = (ulong.MaxValue - (ulong)tweet.CreatedAt.ToBinary()).ToString().PadLeft(20) + tweet.IdStr;
                 var key = word + "_" + time_index;
                 var row = new CellSet.Row { key = Encoding.UTF8.GetBytes(key) };
 
                 var value = new Cell { column = Encoding.UTF8.GetBytes("d:id_str"), data = Encoding.UTF8.GetBytes(tweet.IdStr) };
                 row.values.Add(value);
             }
-            
+
         }
 
         private int CalcSentimentScore(string[] words)
         {
-
+            var total = 0;
+            foreach (var word in words)
+            {
+                if (dictionary.Keys.Contains(word))
+                {
+                    switch (dictionary[word].Polarity)
+                    {
+                        case "negative": total -= 1; break;
+                        case "positive": total += 1; break;
+                    }
+                }
+            }
+            if (total > 0)
+            {
+                return 1;
+            }
+            else if (total < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public static void CreateTweetCells(CellSet set, ITweet tweet)
+        {
+            var key = tweet.IdStr;
+            var row = new CellSet.Row { key = Encoding.UTF8.GetBytes(key) };
+            var value = new Cell { column = Encoding.UTF8.GetBytes("d:created_at"), data = Encoding.UTF8.GetBytes(tweet.CreatedAt.ToLongTimeString()) };
+            row.values.Add(value);
+            value = new Cell { column = Encoding.UTF8.GetBytes("d:text"), data = Encoding.UTF8.GetBytes(tweet.Text) };
+            row.values.Add(value);
+            if (tweet.Coordinates != null)
+            {
+                var str = tweet.Coordinates.Longitude.ToString() + "," + tweet.Coordinates.Latitude.ToString();
+                value = new Cell { column = Encoding.UTF8.GetBytes("d:coor"), data = Encoding.UTF8.GetBytes(str) };
+                row.values.Add(value);
+            }
+            if (tweet.Place != null)
+            {
+                value = new Cell { column = Encoding.UTF8.GetBytes("d:place_fullname"), data = Encoding.UTF8.GetBytes(tweet.Place.FullName) };
+                row.values.Add(value);
+            }
+            set.rows.Add(row);
         }
 
-
-        private object CreateFromFile(string v)
+        private void LoadDictionary()
         {
-            throw new NotImplementedException();
+            List<string> lines = File.ReadAllLines("").ToList();
+            var items = lines.Select(line =>
+            {
+                var fields = line.Split('\t');
+                var pos = 0;
+                return new DictionaryItem
+                {
+                    Type = fields[pos++],
+                    Length = Convert.ToInt32(fields[pos++]),
+                    Word = fields[pos++],
+                    Pos = fields[pos++],
+                    Polarity = fields[pos++]
+
+
+                };
+
+            });
+            dictionary = new Dictionary<string, DictionaryItem>();
+            foreach (var item in items)
+            {
+                if (!dictionary.Keys.Contains(item.Word))
+                {
+                    dictionary.Add(item.Word, item);
+                }
+            }
+
+        }
+        internal static ClusterCredentials CreateFromFile(string path)
+        {
+            List<string> lines = File.ReadAllLines(path).ToList();
+            return CreateFromList(lines);
+        }
+        internal static ClusterCredentials CreateFromList(List<string> lines)
+        {
+            if (lines.Count() != 3)
+            {
+                throw new ArgumentException(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "Expected," +
+                        "first containing" + "Given {0} lines!",
+                        lines.Count()),
+                    "lines");
+            }
+            var rv = new ClusterCredentials(new Uri(lines[0]), lines[1], lines[2]);
+            return rv;
+        }
+        public class DictionaryItem
+        {
+            public string Type { get; set; }
+            public int Length { get; set; }
+            public string Word { get; set; }
+            public string Pos { get; set; }
+            public string Stemmed { get; set; }
+            public string Polarity { get; set; }
         }
     }
 }
